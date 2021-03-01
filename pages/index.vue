cd3fec30bdac9535cd9dd4295148b7ef7d667332
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-03-01 17:28:28 +0100

    working example

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script>
+export default {
+  fetch() {
+    console.log('fetch 33')
+  },
+  asyncData() {
+    console.log('async data 33')
+  },
+}
+</script>
